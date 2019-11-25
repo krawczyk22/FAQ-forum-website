36a7c2132468c7efc3457e6e3cd9638de45dd57a
Michal Krawczykowski <krawczy5@coventry.ac.uk>
2019-11-25 10:18:18 +0000

    updated acceptance tests

--- acceptance tests/ui.test.js
+++ acceptance tests/ui.test.js
@@ -141,6 +141,43 @@ describe('register', () => {
 		done()
 	}, 16000)
 
+	test('register one user missing credential username', async done => {
+		// start generating a trace file
+		//await page.tracing.start({path: 'trace/add_one_item_har.json',screenshots: true})
+		await har.start({ path: 'trace/add_one_item_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:8080/register', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:8080/register', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/registerpage.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', '')
+		await page.type('input[name=pass]', 'testtest')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('h1')
+			return dom.innerText
+		})
+		expect(heading).toBe('An Error Has Occurred')
+
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		//await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+
 	test('register one user invalid credential password', async done => {
 		// start generating a trace file
 		//await page.tracing.start({path: 'trace/add_one_item_har.json',screenshots: true})
@@ -155,6 +192,43 @@ describe('register', () => {
 		// complete the form and click submit
 		await page.type('input[name=user]', 'test')
 		await page.type('input[name=pass]', 'te')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('h1')
+			return dom.innerText
+		})
+		expect(heading).toBe('An Error Has Occurred')
+
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		//await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+
+	test('register one user missing credential password', async done => {
+		// start generating a trace file
+		//await page.tracing.start({path: 'trace/add_one_item_har.json',screenshots: true})
+		await har.start({ path: 'trace/add_one_item_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:8080/register', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:8080/register', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/registerpage.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', 'test')
+		await page.type('input[name=pass]', '')
 		await page.click('input[type=submit]')
 		await page.waitForSelector('h1')
 		await page.waitFor(1000) // sometimes you need a second delay
@@ -255,6 +329,43 @@ describe('login', () => {
 		done()
 	}, 16000)
 
+	test('login one user missing credential username', async done => {
+		// start generating a trace file
+		//await page.tracing.start({path: 'trace/add_login_har.json',screenshots: true})
+		await har.start({ path: 'trace/add_add_login_har_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/loginpage.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', '')
+		await page.type('input[name=pass]', 'testtest')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('h1')
+			return dom.innerText
+		})
+		expect(heading).toBe('An Error Has Occurred')
+
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		//await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+
 	test('login one user invalid credential username length', async done => {
 		// start generating a trace file
 		//await page.tracing.start({path: 'trace/add_login_har.json',screenshots: true})
@@ -306,6 +417,43 @@ describe('login', () => {
 		// complete the form and click submit
 		await page.type('input[name=user]', 'test')
 		await page.type('input[name=pass]', 'testadasdadsasd')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('h1')
+			return dom.innerText
+		})
+		expect(heading).toBe('An Error Has Occurred')
+
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		//await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+
+	test('login one user missing credential password', async done => {
+		// start generating a trace file
+		//await page.tracing.start({path: 'trace/add_login_har.json',screenshots: true})
+		await har.start({ path: 'trace/add_add_login_har_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/loginpage.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', 'test')
+		await page.type('input[name=pass]', '')
 		await page.click('input[type=submit]')
 		await page.waitForSelector('h1')
 		await page.waitFor(1000) // sometimes you need a second delay
@@ -482,6 +630,61 @@ describe('add question', () => {
 		done()
 	}, 16000)
 
+	test('add one question missing credential title', async done => {
+		// start generating a trace file
+		//await page.tracing.start({path: 'trace/add_login_har.json',screenshots: true})
+		await har.start({ path: 'trace/add_add_question_har_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/loginpage.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', 'test')
+		await page.type('input[name=pass]', 'testtest')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('h1')
+			return dom.innerText
+        })
+        
+		expect(heading).toBe('You are logged in. Click this to log out')
+		
+		await page.waitForSelector('h1')
+        await page.waitFor(1000) // sometimes you need a second delay
+		
+		await page.screenshot({ path: 'screenshots/loginpagetoquestioninde.png' })
+
+        await page.type('textarea[name=title]', '')
+		await page.type('textarea[name=description]', 'sample description')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		const heading2 = await page.evaluate( () => {
+			const dom2 = document.querySelector('h1')
+			return dom2.innerText
+		})
+		
+		expect(heading2).toBe('An Error Has Occurred')
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		//await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+
 	test('add one question invalid credential description', async done => {
 		// start generating a trace file
 		//await page.tracing.start({path: 'trace/add_login_har.json',screenshots: true})
@@ -517,6 +720,61 @@ describe('add question', () => {
 
         await page.type('textarea[name=title]', 'sampleTitle')
 		await page.type('textarea[name=description]', 'sa')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		const heading2 = await page.evaluate( () => {
+			const dom2 = document.querySelector('h1')
+			return dom2.innerText
+		})
+		
+		expect(heading2).toBe('An Error Has Occurred')
+		// grab a screenshot
+		const image = await page.screenshot()
+		// compare to the screenshot from the previous test run
+		expect(image).toMatchImageSnapshot()
+		// stop logging to the trace files
+		//await page.tracing.stop()
+		await har.stop()
+		done()
+	}, 16000)
+
+	test('add one question missing credential description', async done => {
+		// start generating a trace file
+		//await page.tracing.start({path: 'trace/add_login_har.json',screenshots: true})
+		await har.start({ path: 'trace/add_add_question_har_trace.har' })
+		// ARRANGE
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		await page.goto('http://localhost:8080/login?msg=you%20need%20to%20log%20in', { timeout: 30000, waitUntil: 'load' })
+		// take a screenshot and save to the file system
+		await page.screenshot({ path: 'screenshots/loginpage.png' })
+
+		// ACT
+		// complete the form and click submit
+		await page.type('input[name=user]', 'test')
+		await page.type('input[name=pass]', 'testtest')
+		await page.click('input[type=submit]')
+		await page.waitForSelector('h1')
+		await page.waitFor(1000) // sometimes you need a second delay
+
+		// ASSERT
+
+		// extracting the text inside the first H1 element on the page
+		const heading = await page.evaluate( () => {
+			const dom = document.querySelector('h1')
+			return dom.innerText
+        })
+        
+		expect(heading).toBe('You are logged in. Click this to log out')
+		
+		await page.waitForSelector('h1')
+        await page.waitFor(1000) // sometimes you need a second delay
+		
+		await page.screenshot({ path: 'screenshots/loginpagetoquestioninde.png' })
+
+        await page.type('textarea[name=title]', 'sampleTitle')
+		await page.type('textarea[name=description]', '')
 		await page.click('input[type=submit]')
 		await page.waitForSelector('h1')
 		await page.waitFor(1000) // sometimes you need a second delay
